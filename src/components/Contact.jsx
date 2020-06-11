678adce15e99d2d003949865b48214d6ce4e1481
Myra <myrajgiddens@aol.com>
2020-06-11 12:06:54 -0400

    Adding form to contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,6 +4,12 @@ const ContactPage = () => {
     return (
         <div>
            <h3> hello from the contact page </h3>
+           <form> 
+               <input type="text" placeholder="First Name" />
+               <input type="e-mail" placeholder="E-mail Address" />
+               <input type="text" placeholder="Your message here" />
+               <button type="submit">Submit</button>
+           </form>
         </div>
     );
 };
